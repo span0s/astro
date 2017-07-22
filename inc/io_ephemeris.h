e6ba9bfd0a5afd332a51510095295c488deb028a
Stephen <Stephen@email.com>
2017-07-22 13:50:46 -0400

    Clean up agi io

--- inc/io_ephemeris.h
+++ inc/io_ephemeris.h
@@ -8,16 +8,16 @@ bool writeEphemToAGI(std::string outfile, Ephemeris& ephem) {
 
     FILE* fp = fopen(outfile.c_str(), "w");
 
-    fprintf(fp, "stk.v.4.0.0\n\n");
-    fprintf(fp, "BEGIN Ephemeris\n");
+    fprintf(fp, "stk.v.4.3\n\n");
+    fprintf(fp, "BEGIN Ephemeris\n\n");
 
-    fprintf(fp, "ScenarioEpoch %s", ephem.states_[0].tc_.getStrAGI().c_str());
+    fprintf(fp, "ScenarioEpoch %s\n", ephem.states_[0].tc_.getStrAGI().c_str());
 
     std::string csystem = "UNKNOWN FRAME";
     switch(ephem.csystem_) {
         case FIXED    : csystem = "FIXED"; break;
         case INERTIAL : csystem = "ICRF";  break;
-        case TEME     : csystem = "TEME";  break;
+        case TEME     : csystem = "TEMEOfEpoch";  break;
     }
     fprintf(fp, "CoordinateSystem %s\n", csystem.c_str());
     if (ephem.csystem_ == TEME) {
@@ -26,17 +26,17 @@ bool writeEphemToAGI(std::string outfile, Ephemeris& ephem) {
 
     fprintf(fp, "NumberOfEphemerisPoints %ld\n", ephem.states_.size());
 
-    fprintf(fp, "EphemerisTimePosVel\n");
+    fprintf(fp, "\nEphemerisTimePosVel\n");
     for (int ii = 0; ii < (int)ephem.states_.size(); ii++) {
         fprintf(
-            fp, "%.6lf %.12lf %.12lf %.12lf %.12lf %.12lf %.12lf\n",
+            fp, "%.6lf %.6lf %.6lf %.6lf %.12lf %.12lf %.12lf\n",
             ephem.states_[ii].tc_ - ephem.states_[0].tc_,
             ephem.states_[ii].pos_.x_, ephem.states_[ii].pos_.y_, ephem.states_[ii].pos_.z_, 
             ephem.states_[ii].vel_.x_, ephem.states_[ii].vel_.y_, ephem.states_[ii].vel_.z_
         );
     }
 
-    fprintf(fp, "END Ephemeris\n");
+    fprintf(fp, "\nEND Ephemeris\n");
 
     fclose(fp);
 
