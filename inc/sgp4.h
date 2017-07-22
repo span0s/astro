821cdbb016981d60ccc4cad2cacc004ad5db587f
Stephen <Stephen@email.com>
2017-07-21 20:16:46 -0400

    Initial TLE work

---
+++ inc/sgp4.h
@@ -0,0 +1,59 @@
+#ifndef ASTRO_SGP4_H
+#define ASTRO_SGP4_H
+
+#include "vallado_sgp4.h"
+#include "timecode.h"
+#include "ephemeris.h"
+
+class TLE{
+    public:
+        TLE(std::string line0, std::string line1) {
+            // Store/convert line data
+            line0_ = line0;
+            line1_ = line1;
+            vector<char> cstr1(line0.c_str(), line0.c_str() + line0.size() + 1);
+            vector<char> cstr2(line1.c_str(), line1.c_str() + line1.size() + 1);
+
+            // Vallado inputs
+            char typerun = 'c';
+            char typeinput = 'm';
+            Vallado::gravconsttype whichconst = Vallado::wgs72;
+
+            // Initialize element set
+            double startmfe, stopmfe, deltamin;
+            Vallado::twoline2rv(
+                cstr1.data(), cstr2.data(), typerun, typeinput, 'a', whichconst,
+                startmfe, stopmfe, deltamin, satrec_
+            );
+
+            // Set epoch
+            DateTime dt;
+            Vallado::invjday(
+                satrec_.jdsatepoch, satrec_.jdsatepochF,
+                dt.year, dt.month, dt.day, dt.hour, dt.min, dt.secs
+            );
+            epoch_ = Timecode(dt);
+        }
+
+        StateVec getState(Timecode tc) {
+            double pos[3], vel[3];
+            Vallado::sgp4(satrec_, tc - epoch_, pos, vel);
+            return StateVec(
+                tc, Vec3(pos[0], pos[1], pos[2]), Vec3(vel[0], vel[1], vel[2])
+            );
+        }
+
+    public:
+        string line0_, line1_;
+        Timecode epoch_;
+        Vallado::elsetrec satrec_;
+};
+
+// Load TLE's from files
+// Find TLE's for satid
+// Create ephem from tle
+//Ephemeris ephemFromTle(TLE tle, Timecode tc0, Timecode tc1, double dt) {
+//
+//}
+
+#endif
---
+++ test/sgp4.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include "sgp4.h"
+
+int main(int argc, char *argv[]) {
+
+    string str1 = "1 28868U 05036A   17189.60254437 -.00000076 +00000-0 +00000-0 0  9997";
+    string str2 = "2 28868 000.0215 332.1778 0003279 099.8260 324.3314 01.00271962013763";
+
+    vector<char> cstr1(str1.c_str(), str1.c_str() + str1.size() + 1);
+    vector<char> cstr2(str2.c_str(), str2.c_str() + str2.size() + 1);
+
+    Vallado::elsetrec satrec;
+    double startmfe, stopmfe, deltamin;
+    Vallado::gravconsttype whichconst = Vallado::wgs72;
+
+    char typerun = 'c';
+    char typeinput = 'm';
+    Vallado::twoline2rv(
+        cstr1.data(), cstr2.data(), typerun, typeinput, 'a', whichconst,
+        startmfe, stopmfe, deltamin, satrec
+    );
+
+    cout << satrec.satnum
+         << " " << satrec.classification
+         << " " << satrec.intldesg
+         << " " << satrec.epochyr
+         << " " << satrec.epochdays
+         << " " << satrec.ndot
+         << " " << satrec.nddot
+         << " " << satrec.bstar
+         << " " << satrec.ephtype
+         << " " << satrec.elnum
+         << endl;
+
+    cout << satrec.satnum
+         << " " << satrec.inclo
+         << " " << satrec.nodeo
+         << " " << satrec.ecco
+         << " " << satrec.argpo
+         << " " << satrec.mo
+         << " " << satrec.no_kozai
+         << " " << satrec.revnum
+         << endl;
+
+    double pos[3];
+    double vel[3];
+    Vallado::sgp4(satrec, 1.0, pos, vel);
+    cout << pos[0] << ", " << pos[1] << ", " << pos[2] << endl;
+    cout << vel[0] << ", " << vel[1] << ", " << vel[2] << endl;
+
+    TLE tle = TLE(str1, str2);
+    StateVec sv = tle.getState(tle.epoch_ + 1);
+    cout << sv.getStr() << endl;
+
+    return 0;
+}
