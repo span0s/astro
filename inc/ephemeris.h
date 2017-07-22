0d7e15b223ebc8a6fe9ae9affbb53a4189e555c4
Stephen <Stephen@email.com>
2017-07-21 20:15:50 -0400

    Add initial ephemeris class

---
+++ inc/ephemeris.h
@@ -0,0 +1,48 @@
+#ifndef ASTRO_EPHEMERIS_H
+#define ASTRO_EPHEMERIS_H
+
+#include "timecode.h"
+#include "vecmat3.h"
+
+class StateVec {
+    public:
+        StateVec(){}
+
+        StateVec(Timecode tc, Vec3 pos) {
+            tc_ = tc;
+            pos_ = pos;
+        }
+
+        StateVec(Timecode tc, Vec3 pos, Vec3 vel) {
+            tc_ = tc;
+            pos_ = pos;
+            vel_ = vel;
+        }
+
+        StateVec(Timecode tc, Vec3 pos, Vec3 vel, Vec3 acc) {
+            tc_ = tc;
+            pos_ = pos;
+            vel_ = vel;
+            acc_ = acc;
+        }
+
+        std::string getStr() {
+            std::ostringstream strs;
+            strs << "TC:  " << tc_.getStr() << "\nPos: " << pos_.getStr()
+                 << "\nVel: " << vel_.getStr() << "\nAcc: " << acc_.getStr();
+            return strs.str();
+        }
+
+        Timecode tc_;
+        Vec3 pos_, vel_, acc_;
+};
+
+class Ephemeris {
+    public:
+        Ephemeris();
+
+    private:
+        std::vector<StateVec> states_;
+};
+
+#endif
